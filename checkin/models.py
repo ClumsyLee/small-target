36ea732694c40ff03c6418ba334189537c66f605
Thomas Lee <lisihan969@gmail.com>
2016-10-18 15:21:22 +0800

    Add model

--- checkin/models.py
+++ checkin/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Checkin(models.Model):
+    student_id = models.CharField(max_length=16)
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return '{} @ {}'.format(self.student_id, self.time)
