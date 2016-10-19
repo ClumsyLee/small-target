28e13d97d1f521d5598954b5e95379c0cf4b9bf7
stariver <wangmh15@gmail.com>
2016-10-19 15:01:59 +0800

    完成初步

--- checkin/models.py
+++ checkin/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=10)
+    lastcheckintime = models.DateField(default=timezone.now)
+    checkintimes = models.IntegerField()
+    def __str__(self):
+        return self.student_id
 
 class Checkin(models.Model):
-    student_id = models.CharField(max_length=16)
-    time = models.DateTimeField()
+    student = models.ForeignKey(Student,related_name='record',on_delete=models.CASCADE,null = True)
+    time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return '{} @ {}'.format(self.student_id, self.time)
