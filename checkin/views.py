922719f160283637d167fbe648fba347ec335b6c
Thomas Lee <lisihan969@gmail.com>
2016-10-20 21:34:43 +0800

    Show only checkin num on index

--- checkin/views.py
+++ checkin/views.py
@@ -10,21 +10,21 @@ def index(request):
         form = CheckinForm(request.POST)
         if form.is_valid():
             student_id = form.cleaned_data['student_id']
-        try:
-            student = Student.objects.get(student_id = student_id)
-            if (student.lastcheckintime == datetime.date.today()):
-                return HttpResponse("你今天已经签过到！")
-            student.checkintimes = student.checkintimes+1
-            student.lastcheckintime = datetme.date.today()
-            student.save()
-            checkinrecord = Checkin(student = student)
-            checkinrecord.save()
-        except:
-            student = Student(student_id = student_id,checkintimes = 1)
-            student.save()
-            checkinrecord = Checkin(student = student)
-            checkinrecord.save()
-        return HttpResponse("签到成功")
+            try:
+                student = Student.objects.get(student_id = student_id)
+                if (student.lastcheckintime == datetime.date.today()):
+                    return HttpResponse("你今天已经签过到！")
+                student.checkintimes = student.checkintimes+1
+                student.lastcheckintime = datetme.date.today()
+                student.save()
+                checkinrecord = Checkin(student = student)
+                checkinrecord.save()
+            except:
+                student = Student(student_id = student_id,checkintimes = 1)
+                student.save()
+                checkinrecord = Checkin(student = student)
+                checkinrecord.save()
+            return HttpResponse("签到成功")
     else:
         form = CheckinForm()
     try:
