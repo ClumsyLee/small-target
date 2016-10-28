207ee8581f2e67a80a159a4dc4bedc91ff53dd3a
Thomas Lee <lisihan969@gmail.com>
2016-10-28 11:05:40 +0800

    Bug fixes

--- smalltarget/settings.py
+++ smalltarget/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '{{ SECRET_KEY }}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'small-target.thu.ee'
+]
 
 
 # Application definition
