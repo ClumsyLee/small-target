9352380a264195e6f77d72da036f2b412f7dddcc
Thomas Lee <lisihan969@gmail.com>
2016-10-28 10:41:17 +0800

    DEBUG = False

--- smalltarget/urls.py
+++ smalltarget/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('checkin.urls')),
@@ -22,6 +24,7 @@ urlpatterns = [
 ]
 
 if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
